3ff9dbdf6595f60a21ed2a0ad36c99d9bf59fc1b
fatihkerim <fatihkerim@gmail.com>
2019-07-21 12:16:18 +0300

    Add project files.

---
+++ MathService/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathService
+{
+    public class Common
+    {
+        public bool IsNegative(int number)
+        {
+            return false;
+        }
+
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
